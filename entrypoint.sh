e1229ab7c2f399690e7589339df61b2291e2d217
Raghu K <raghuk.vit@gmail.com>
2026-07-03 07:24:01 +0530

    config: update 2 file(s) (docker-compose.yml,entrypoint.sh)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
+az login --service-principal --username ${az_username} --password ${az_password} --tenant ${az_tenant} --subscription ${az_subscription}
 rm -f .runner
 ./config.sh --url https://github.com/raghudevopsb89 --unattended --replace --name ${RUNNER_NAME} --token ${TOKEN}
 ./run.sh
