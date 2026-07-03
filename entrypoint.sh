899634ca6edf80c1b4bee4c330d98fb45fe2787a
Raghu K <raghuk.vit@gmail.com>
2026-07-03 07:35:27 +0530

    scripts: update 1 file(s) (entrypoint.sh)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,3 @@
-env
-tail -f /dev/null
 az login --service-principal --username ${az_username} --password ${az_password} --tenant ${az_tenant} --subscription ${az_subscription}
 rm -f .runner
 ./config.sh --url https://github.com/raghudevopsb89 --unattended --replace --name ${RUNNER_NAME} --token ${TOKEN}
